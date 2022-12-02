e73973f48ebab77da5112316308327f464285a2b
kulkard1 <DJ.Kulkarni@moodys.com>
2022-12-02 13:39:09 -0500

    minor day1 cleanup

--- src/main/java/com/kulkard1/advent2022/day1/Day1Solution1.java
+++ src/main/java/com/kulkard1/advent2022/day1/Day1Solution1.java
@@ -6,22 +6,21 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.stream.Stream;
 
-public class Day1Problem2 {
+public class Day1Solution1 {
+
     public static void main(String... args) throws IOException, URISyntaxException {
         try (final Stream<String> linesStream = Files.lines(Paths.get(ClassLoader.getSystemResource("day-1-input").toURI()))) {
-            final int sumTop3 = Flux.fromStream(linesStream)
+            final int max = Flux.fromStream(linesStream)
                     .bufferUntil(String::isEmpty)
                     .map(list -> list.stream().filter(line -> !line.isEmpty()).mapToInt(Integer::parseInt).sum())
                     .toStream()
-                    .sorted(Collections.reverseOrder())
-                    .limit(3)
                     .mapToInt(i -> i)
-                    .sum();
+                    .max()
+                    .orElseThrow();
 
-            System.out.println("sumTop3 = " + sumTop3);
+            System.out.println("max = " + max);
         }
 
     }
--- src/main/java/com/kulkard1/advent2022/day1/Day1Solution2.java
+++ src/main/java/com/kulkard1/advent2022/day1/Day1Solution2.java
@@ -6,21 +6,22 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.stream.Stream;
 
-public class Day1Problem1 {
-
+public class Day1Solution2 {
     public static void main(String... args) throws IOException, URISyntaxException {
         try (final Stream<String> linesStream = Files.lines(Paths.get(ClassLoader.getSystemResource("day-1-input").toURI()))) {
-            final int max = Flux.fromStream(linesStream)
+            final int sumTop3 = Flux.fromStream(linesStream)
                     .bufferUntil(String::isEmpty)
                     .map(list -> list.stream().filter(line -> !line.isEmpty()).mapToInt(Integer::parseInt).sum())
                     .toStream()
+                    .sorted(Collections.reverseOrder())
+                    .limit(3)
                     .mapToInt(i -> i)
-                    .max()
-                    .orElseThrow();
+                    .sum();
 
-            System.out.println("max = " + max);
+            System.out.println("sumTop3 = " + sumTop3);
         }
 
     }

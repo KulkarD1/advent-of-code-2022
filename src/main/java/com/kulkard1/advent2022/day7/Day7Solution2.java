bf60c0b3898f5fff77bb4a2a8e0b0bb20f9a3275
kulkard1 <DJ.Kulkarni@moodys.com>
2022-12-07 16:20:44 -0500

    one line rules

--- src/main/java/com/kulkard1/advent2022/day7/Day7Solution2.java
+++ src/main/java/com/kulkard1/advent2022/day7/Day7Solution2.java
@@ -13,12 +13,17 @@ public class Day7Solution2 {
 
     public static void main(String... args) throws IOException, URISyntaxException {
         try (final Stream<String> linesStream = Files.lines(Paths.get(ClassLoader.getSystemResource("day-7-input").toURI()))) {
-            final File anyDirectory = linesStream
-                    .map(Command::new)
-                    .reduce(new File(true, "/", -1, null), (currentFileSystem, command) -> command.execute(currentFileSystem), (a, b) -> a);
-            long availableSpace = 70000000L - anyDirectory.getRoot().getSize();
+
+            final File fileSystemRoot = linesStream
+                                            .map(Command::new)
+                                            .reduce(new File(true, "/", -1, null),
+                                                    (currentFileSystem, command) -> command.execute(currentFileSystem),
+                                                    (fileSystem1, fileSystem2) -> fileSystem1.getRoot())
+                                            .getRoot();
+            long availableSpace = 70000000L - fileSystemRoot.getRoot().getSize();
             long requiredFreeUp = 30000000L - availableSpace;
-            final Long requiredSize = anyDirectory.getRoot().getDirectories()
+            final Long requiredSize = fileSystemRoot
+                    .getDirectories()
                     .stream()
                     .sorted(Comparator.comparing(File::getSize))
                     .map(File::getSize)

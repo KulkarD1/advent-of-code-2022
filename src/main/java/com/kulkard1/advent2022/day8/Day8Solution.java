8d26e64463dee3bbd2a15a191c7cc7a1fbbb7cda
kulkard1 <DJ.Kulkarni@moodys.com>
2022-12-08 14:18:36 -0500

    minor cleanup

--- src/main/java/com/kulkard1/advent2022/day8/Day8Solution.java
+++ src/main/java/com/kulkard1/advent2022/day8/Day8Solution.java
@@ -17,8 +17,8 @@ public class Day8Solution {
     public static void main(String... args) throws IOException, URISyntaxException {
         try (final Stream<String> linesStream = Files.lines(Paths.get(ClassLoader.getSystemResource("day-8-input").toURI()))) {
             final List<List<Integer>> grid = linesStream
-                    .map(line -> Arrays.stream(line.split("")).mapToInt(Integer::valueOf).boxed().toList())
-                    .toList();
+                .map(line -> Arrays.stream(line.split("")).mapToInt(Integer::valueOf).boxed().toList())
+                .toList();
 
             Flux.range(0, grid.size())
                 .flatMap(rowIndex -> Flux.combineLatest(gridCoordinates -> new Tree((Integer) gridCoordinates[0], (Integer) gridCoordinates[1], grid.get((Integer) gridCoordinates[0]).get((Integer) gridCoordinates[1])), Mono.just(rowIndex), Flux.range(0, grid.get(rowIndex).size())))
@@ -28,12 +28,12 @@ public class Day8Solution {
                 .subscribe();
 
             final long maxScenicScore = Flux.range(0, grid.size())
-                    .flatMap(rowIndex -> Flux.combineLatest(gridCoordinates -> new Tree((Integer) gridCoordinates[0], (Integer) gridCoordinates[1], grid.get((Integer) gridCoordinates[0]).get((Integer) gridCoordinates[1])), Mono.just(rowIndex), Flux.range(0, grid.get(rowIndex).size())))
-                    .map(tree -> tree.getScenicScore(grid))
-                    .toStream()
-                    .mapToLong(i -> i)
-                    .max()
-                    .orElseThrow();
+                .flatMap(rowIndex -> Flux.combineLatest(gridCoordinates -> new Tree((Integer) gridCoordinates[0], (Integer) gridCoordinates[1], grid.get((Integer) gridCoordinates[0]).get((Integer) gridCoordinates[1])), Mono.just(rowIndex), Flux.range(0, grid.get(rowIndex).size())))
+                .map(tree -> tree.getScenicScore(grid))
+                .toStream()
+                .mapToLong(i -> i)
+                .max()
+                .orElseThrow();
             assertEquals(313200, maxScenicScore);
         }
     }

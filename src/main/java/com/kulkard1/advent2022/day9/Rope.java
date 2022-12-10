5045e148cd0053bf228b4b1e52176982339a6d9b
kulkard1 <DJ.Kulkarni@moodys.com>
2022-12-10 10:02:33 -0500

    simplify

--- src/main/java/com/kulkard1/advent2022/day9/Rope.java
+++ src/main/java/com/kulkard1/advent2022/day9/Rope.java
@@ -10,7 +10,6 @@ import java.util.stream.IntStream;
 
 @Data
 public class Rope {
-    private GridCoordinate headPosition;
 
     private List<GridCoordinate> tailPosition = new ArrayList<>();
 
@@ -19,8 +18,7 @@ public class Rope {
     public Rope(int knots) {
         int row = 5000;
         int column = 1000;
-        headPosition = new GridCoordinate(row, column);
-        IntStream.range(1, knots).forEach(i -> tailPosition.add(new GridCoordinate(row, column)));
+        IntStream.range(0, knots).forEach(i -> tailPosition.add(new GridCoordinate(row, column)));
         TAIL_VISITS.add(new GridCoordinate(row, column));
     }
 
@@ -34,24 +32,23 @@ public class Rope {
     private Rope move(String direction, Integer steps) {
         if(steps > 0) {
             switch (direction) {
-                case "R" -> headPosition.setColumn(headPosition.getColumn() + 1);
-                case "L" -> headPosition.setColumn(headPosition.getColumn() - 1);
-                case "U" -> headPosition.setRow(headPosition.getRow() - 1);
-                case "D" -> headPosition.setRow(headPosition.getRow() + 1);
+                case "R" -> tailPosition.get(0).setColumn(tailPosition.get(0).getColumn() + 1);
+                case "L" -> tailPosition.get(0).setColumn(tailPosition.get(0).getColumn() - 1);
+                case "U" -> tailPosition.get(0).setRow(tailPosition.get(0).getRow() - 1);
+                case "D" -> tailPosition.get(0).setRow(tailPosition.get(0).getRow() + 1);
                 default -> throw new UnsupportedOperationException();
             }
-            moveTail(0);
+            moveTail(1);
             move(direction, steps - 1);
         }
         return this;
     }
 
     private void moveTail(int position) {
-        GridCoordinate movedHeadPosition = position == 0 ? headPosition : tailPosition.get(position - 1);
+        GridCoordinate movedHeadPosition = tailPosition.get(position - 1);
         GridCoordinate tailPositionToMove = tailPosition.get(position);
         position++;
         if(Math.abs(tailPositionToMove.getColumn() - movedHeadPosition.getColumn()) == 2) {
-            //move 1 horizontal
             if (tailPositionToMove.getColumn() > movedHeadPosition.getColumn()) {
                 tailPositionToMove.moveLeft();
             } else {
@@ -66,7 +63,6 @@ public class Rope {
             }
         }
         if(Math.abs(tailPositionToMove.getRow() - movedHeadPosition.getRow()) == 2) {
-            //move 1 vertical
             if (tailPositionToMove.getRow() > movedHeadPosition.getRow()) {
                 tailPositionToMove.moveUp();
             } else {

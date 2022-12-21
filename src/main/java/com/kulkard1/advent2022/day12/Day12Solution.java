7c41ceeb12398b4705e3d4d00fce3abcc756c556
kulkard1 <DJ.Kulkarni@moodys.com>
2022-12-21 11:10:58 -0500

    Day 12 example solution

---
+++ src/main/java/com/kulkard1/advent2022/day12/Day12Solution.java
@@ -0,0 +1,51 @@
+package com.kulkard1.advent2022.day12;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Day12Solution {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        try (final Stream<String> linesStream = Files.lines(Paths.get(ClassLoader.getSystemResource("day-12-input").toURI()))) {
+            AtomicReference<GridCoordinate> start = new AtomicReference<>();
+            List<List<GridCoordinate>> grid = new ArrayList<>();
+            linesStream.forEach(line -> {
+                final List<GridCoordinate> row = IntStream.range(0, line.length())
+                        .mapToObj(column -> new GridCoordinate(grid.size(), column, line.charAt(column)))
+                        .peek(gridCoordinate -> {
+                            if(gridCoordinate.getValue() == 83) {
+                                start.set(gridCoordinate);
+                            }
+                        })
+                        .collect(Collectors.toList());
+                grid.add(row);
+            });
+            //System.out.println("grid = " + grid);
+            GridCoordinate end = search(start.get(), grid).orElseThrow();
+            System.out.println(end);
+        }
+    }
+
+    public static Optional<GridCoordinate> search(GridCoordinate root, List<List<GridCoordinate>> grid) {
+        Queue<GridCoordinate> queue = new ArrayDeque<>();
+
+        queue.add(root);
+        GridCoordinate currentCoordinate;
+        while(!queue.isEmpty()) {
+            currentCoordinate = queue.remove();
+            if(currentCoordinate.getValue() == 69) {
+                return Optional.of(currentCoordinate);
+            } else {
+                currentCoordinate.setVisited(true);
+                queue.addAll(currentCoordinate.getPossiblePaths(grid));
+            }
+        }
+        return Optional.empty();
+    }
+}
---
+++ src/main/java/com/kulkard1/advent2022/day12/GridCoordinate.java
@@ -0,0 +1,83 @@
+package com.kulkard1.advent2022.day12;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+public class GridCoordinate {
+    private final Integer row;
+
+    private final Integer column;
+
+    private final int value;
+
+    private boolean visited;
+
+    private int depth;
+
+    public int getElevation() {
+        return switch (value) {
+            case 83 ->  97;
+            case 69 ->  122;
+            default -> value;
+        };
+    }
+
+    public Optional<GridCoordinate> moveRight(List<List<GridCoordinate>> grid) {
+        if(column + 1 < grid.get(row).size()) {
+            GridCoordinate right = grid.get(row).get(column + 1);
+            if(!right.isVisited() && right.getElevation() - this.getElevation() < 2) {
+                return Optional.of(right);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<GridCoordinate> moveLeft(List<List<GridCoordinate>> grid) {
+        if(column - 1 >= 0) {
+            GridCoordinate left = grid.get(row).get(column - 1);
+            if(!left.isVisited() && left.getElevation() - this.getElevation() < 2) {
+                return Optional.of(left);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<GridCoordinate> moveUp(List<List<GridCoordinate>> grid) {
+        if(row - 1 >= 0) {
+            GridCoordinate up = grid.get(row - 1).get(column);
+            if(!up.isVisited() && up.getElevation() - this.getElevation() < 2) {
+                return Optional.of(up);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Optional<GridCoordinate> moveDown(List<List<GridCoordinate>> grid) {
+        if(row + 1 < grid.size()) {
+            GridCoordinate down = grid.get(row + 1).get(column);
+            if(!down.isVisited() && down.getElevation() - this.getElevation() < 2) {
+                return Optional.of(down);
+            }
+        }
+        return Optional.empty();
+    }
+
+    public Collection<? extends GridCoordinate> getPossiblePaths(List<List<GridCoordinate>> grid) {
+        List<GridCoordinate> possibleMoves = new ArrayList<>();
+        Optional<GridCoordinate> right = moveRight(grid);
+        Optional<GridCoordinate> left = moveLeft(grid);
+        Optional<GridCoordinate> up = moveUp(grid);
+        Optional<GridCoordinate> down = moveDown(grid);
+        right.ifPresent(possibleMoves::add);
+        left.ifPresent(possibleMoves::add);
+        up.ifPresent(possibleMoves::add);
+        down.ifPresent(possibleMoves::add);
+        possibleMoves.forEach(gridCoordinate -> gridCoordinate.setDepth(this.getDepth() + 1));
+        return possibleMoves;
+    }
+}
